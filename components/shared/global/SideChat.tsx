8e6a1aef5b632bc9898386dbf537a7232ef79c27
Warren Phillips <38818772+warrenphilly@users.noreply.github.com>
2025-01-16 08:47:38 -0500

    ui updates

--- components/shared/global/SideChat.tsx
+++ components/shared/global/SideChat.tsx
@@ -6,10 +6,12 @@ import {
   getSideChat,
   saveSideChat,
   updateSideChat,
+  
 } from "@/lib/firebase/firestore";
 import { Image, Upload } from "lucide-react"; // Import icons
 import React, { useEffect, useRef, useState } from "react";
-
+import { useRouter } from "next/navigation";
+import { useUser } from "@clerk/nextjs";
 // First, let's define our message types
 interface Sentence {
   id: number;
@@ -54,6 +56,8 @@ const SideChat = ({
   const [isLoading, setIsLoading] = useState(true);
   const [primeSentence, setPrimeSentenceLocal] = useState<string | null>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const { user } = useUser();
 
   // Update both local and parent state when prime sentence changes
   const updatePrimeSentence = (newPrimeSentence: string | null) => {
@@ -229,8 +233,17 @@ const SideChat = ({
 
   return (
     <div className="flex flex-col h-full w-full border-3 bg-slate-100 rounded-2xl mb-4 max-h-[90vh] p-3 overflow-y-auto">
+      <div className="grid grid-cols-3 items-center ">
+      <Button
+            className="bg-slate-100 hover:bg-red-600 text-white w-fit ml-4 rounded-full border border-red-600 text-red-600"
+            onClick={handleClearChat}
+            disabled={!sideChatId}
+          >
+            Clear Chat
+          </Button>
       <div className="flex flex-row items-center justify-center">
-        <h1 className="text-xl font-regular text-[#94b347]">Talk to Notes</h1>
+        <h1 className="text-xl font-semibold text-[#94b347]">Talk to Notes</h1>
+      </div>
       </div>
 
       <div className=" bg-slate-200 text-[#94b347] rounded-lg   p-4 m-4">
@@ -242,35 +255,49 @@ const SideChat = ({
           )}
         </div>
         <div className="h-px bg-slate-300" />
+        <div className="flex flex-col items-center justify-center mt-4">
+          <h1 className="text-sm font-semibold text-slate-400">Actions</h1>
+        </div>
+        
         <div className="flex flex-row gap-2 w-full items-center justify-center m-4">
           <Button
-            className="bg-[#94b347] text-white"
-            onClick={() => sendMessage("Explain this in greater detail")}
+            className="bg-slate-200 border border-slate-400 text-slate-400 hover:bg-slate-100 rounded-full shadow-none "
+            onClick={() => sendMessage("Explain this in simple terms to make it easier to understand")}
             disabled={initialPrimeSentence == null}
           >
             Explain{" "}
           </Button>
           <Button
-            className="bg-[#94b347] text-white"
+            className="bg-slate-200 border border-slate-400 text-slate-400 hover:bg-slate-100 rounded-full shadow-none "
+            onClick={() => sendMessage("Expand on this in greater detail")}
+            disabled={initialPrimeSentence == null}
+          >
+            Expand{" "}
+          </Button>
+          <Button
+            className="bg-slate-200 border border-slate-400 text-slate-400 hover:bg-slate-100 rounded-full shadow-none "
             onClick={() => sendMessage("Give me a step-by-step example")}
             disabled={initialPrimeSentence == null}
           >
-            Give Example{" "}
+            Example{" "}
           </Button>
           <Button
-            className="bg-[#94b347] text-white"
+            className="bg-slate-200 border border-slate-400 text-slate-400 hover:bg-slate-100 rounded-full shadow-none "
+            onClick={() => sendMessage("Give me a step-by-step example")}
+            disabled={initialPrimeSentence == null}
+          >
+            Reword{" "}
+          </Button>
+          
+          
+          <Button
+            className="bg-slate-200 border border-slate-400 text-slate-400 hover:bg-slate-100 rounded-full shadow-none "
             onClick={() => setPrimeSentence(null)}
             disabled={initialPrimeSentence == null}
           >
             Clear Context{" "}
           </Button>
-          <Button
-            className="bg-red-500 hover:bg-red-600 text-white"
-            onClick={handleClearChat}
-            disabled={!sideChatId}
-          >
-            Clear Chat
-          </Button>
+        
         </div>
       </div>
       <div className="flex-grow overflow-y-auto p-4 auto-scroll">
@@ -282,58 +309,19 @@ const SideChat = ({
                 key={index}
                 className={`p-2 rounded mb-2 ${
                   msg.user === "User"
-                    ? "border border-slate-400 shadow-sm rounded-lg text-slate-400"
+                    ? "border border-slate-400 shadow-sm rounded-lg text-slate-600"
                     : "border border-[#94b347] text-lg text-[#94b347] my-4 rounded-lg"
                 }`}
               >
                 {msg.user === "AI" ? (
-                  <div className="text-sm">
-                    <strong className="block mb-4 text-lg">{msg.user}:</strong>
-                    {Array.isArray(msg.text) ? (
-                      <div className="space-y-6 ">
-                        {msg.text.map(
-                          (section: Section, sectionIdx: number) => (
-                            <div
-                              key={sectionIdx}
-                              className="bg-white p-4 rounded-lg "
-                            >
-                              {/* <h3 className="text-lg font-bold text-gray-100 mb-3">
-                                {section.title}
-                              </h3> */}
-                              <div className="space-y-2">
-                                {section.sentences.map((sentence: Sentence) => (
-                                  <div className="p-2 bg-gray-50 hover:bg-gray-100 rounded cursor-pointer transition-colors">
-                                    <p className="text-gray-100">
-                                      {sentence.text}
-                                    </p>
-                                  </div>
-                                ))}
-                              </div>
-                            </div>
-                          )
-                        )}
-                      </div>
-                    ) : (
-                      <div className="text-sm bg-white p-2 rounded-lg ">
-                        {String(msg.text)}
-                      </div>
-                    )}
-                  </div>
+                  <div className="text-md font-semibold ">
+                  <strong>Mr. Beck (AI):</strong> <span>{String(msg.text)}</span>
+                
+                </div>
                 ) : (
-                  <div className="text-sm">
-                    <strong>{msg.user}:</strong> <span>{String(msg.text)}</span>
-                    {msg.files && (
-                      <div className="mt-2 flex flex-wrap gap-2">
-                        {msg.files.map((fileUrl: string, idx: number) => (
-                          <img
-                            key={idx}
-                            src={fileUrl}
-                            alt={`Uploaded file ${idx + 1}`}
-                            className="max-w-[200px] max-h-[200px] object-cover rounded"
-                          />
-                        ))}
-                      </div>
-                    )}
+                  <div className="text-md font-semibold">
+                    <strong>{user?.firstName}:</strong> <span>{String(msg.text)}</span>
+                  
                   </div>
                 )}
               </div>
