93091a2e15eefb1ee002869b4adb5c573d4c780c
warrenphilly <38818772+warrenphilly@users.noreply.github.com>
2025-02-05 10:24:07 -0500

    very good

--- components/browser-tabs.tsx
+++ components/browser-tabs.tsx
@@ -10,6 +10,13 @@ import {
 } from "@/components/ui/dialog";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
   addPageToNotebook,
   Page,
   togglePageOpenState,
@@ -17,7 +24,7 @@ import {
 import { Message } from "@/lib/types";
 import { cn } from "@/lib/utils";
 import { AnimatePresence, motion } from "framer-motion";
-import { List, Plus, X } from "lucide-react";
+import { List, Pencil, Plus, X } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import ChatClient from "./shared/chat/ChatClient";
 import { TitleEditor } from "./shared/chat/title-editor";
@@ -184,53 +191,109 @@ export const BrowserTabs: React.FC<BrowserTabsProps> = ({
     }
   };
 
-  return (
-    <div className={cn("w-full h-full mx-auto rounded-lg bg-white flex flex-col items-start justify-center pt-16 ", className)}>
-      <div className="flex items-center z-50  h-fit w-full rounded-t-lg scrollbar-hide ">
-        {tabs.map((tab) => (
-          <motion.div
-            key={tab.id}
+  const isMobileScreen = () =>
+    typeof window !== "undefined" && window.innerWidth < 768;
 
-            layout
-            className={cn(
-              "  flex items-center px-1.5 md:px-3 py-0.5 md:py-2 text-[10px] md:text-sm font-medium relative top-[1px] rounded-t-lg cursor-pointer min-w-[80px] md:min-w-[150px] max-w-[120px] md:max-w-none",
-              activeTabId === tab.id
-                ? "text-foreground border-t border-b border-b-white border-r border-r-slate-300 border-l border-l-slate-300 bg-white shadow-x-md"
-                : "text-muted-foreground bg-slate-100 border-t border-b border-b-white border-r border-r-slate-300 border-l border-l-slate-300"
-            )}
-            onClick={() => setActiveTabId(tab.id)}
+  return (
+    <div
+      className={cn(
+        "w-full h-full mx-auto rounded-lg bg-white flex flex-col items-start justify-center pt-16 ",
+        className
+      )}
+    >
+      <div className="flex items-center z-50 h-fit w-full rounded-t-lg scrollbar-hide">
+        <div className="md:hidden flex justify-between w-full items-center gap-2 py-2  ">
+          <Select
+            value={activeTabId}
+            onValueChange={(value) => setActiveTabId(value)}
           >
-            <TitleEditor
-              initialTitle={tab.title}
-              noteId={tab.id}
-              notebookId={notebookId}
-            />
+            <SelectTrigger className="w-full max-w-[200px]">
+              <SelectValue placeholder="Select a page" />
+            </SelectTrigger>
+            <SelectContent>
+              {tabs.map((tab) => (
+                <SelectItem key={tab.id} value={tab.id}>
+                  {tab.title}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <div className="flex items-center gap-2 ">
+          {activeTab && (
             <button
-              className="ml-0.5 md:ml-2 text-muted-foreground hover:text-foreground"
-              onClick={(e) => {
-                e.stopPropagation();
-                removeTab(tab.id);
+              className="p-2 text-muted-foreground hover:text-foreground"
+              onClick={() => {
+                const titleEditor = document.querySelector(
+                  `[data-page-id="${activeTab.id}"]`
+                );
+                if (titleEditor) {
+                  (titleEditor as HTMLElement).focus();
+                }
               }}
             >
-              <X size={10} className="md:w-4 md:h-4" />
+              <Pencil size={16} />
             </button>
-          </motion.div>
-        ))}
-        <button
-          className="p-0.5 md:p-1 ml-1 md:ml-2 text-muted-foreground group relative flex items-center"
-          onClick={addTab}
-        >
-          <Plus size={14} className="md:w-5 md:h-5" />
-          <span className="absolute left-full ml-2 hidden group-hover:flex transition-opacity whitespace-nowrap text-[10px] md:text-sm text-slate-400">
-            Add page
-          </span>
-        </button>
-        <button
-          className="p-0.5 md:p-1 ml-auto mr-1 md:mr-2 text-muted-foreground hover:text-foreground"
-          onClick={() => setIsModalOpen(true)}
-        >
-          <List size={14} className="md:w-5 md:h-5" />
-        </button>
+          )}
+          <button
+            className="p-2 text-muted-foreground hover:text-foreground"
+            onClick={addTab}
+          >
+            <Plus size={16} />
+          </button>
+          <button
+            className="p-2 text-muted-foreground hover:text-foreground"
+            onClick={() => setIsModalOpen(true)}
+          >
+            <List size={16} />
+          </button>
+</div>
+
+        </div>
+        <div className="hidden md:flex items-center w-full">
+          {tabs.map((tab) => (
+            <motion.div
+              key={tab.id}
+              layout
+              className={cn(
+                "flex items-center px-3 py-2 text-sm font-medium relative top-[1px] rounded-t-lg cursor-pointer min-w-[150px] max-w-none",
+                activeTabId === tab.id
+                  ? "text-foreground border-t border-b border-b-white border-r border-r-slate-300 border-l border-l-slate-300 bg-white shadow-x-md"
+                  : "text-muted-foreground bg-slate-100 border-t border-b border-b-white border-r border-r-slate-300 border-l border-l-slate-300"
+              )}
+              onClick={() => setActiveTabId(tab.id)}
+            >
+              <TitleEditor
+                initialTitle={tab.title}
+                noteId={tab.id}
+                notebookId={notebookId}
+              />
+              <button
+                className="ml-2 text-muted-foreground hover:text-foreground"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  removeTab(tab.id);
+                }}
+              >
+                <X size={14} className="w-4 h-4" />
+              </button>
+            </motion.div>
+          ))}
+          <button
+            className="p-1 ml-2 text-muted-foreground group relative flex items-center"
+            onClick={addTab}
+          >
+            <Plus size={14} className="w-5 h-5" />
+            <span className="absolute left-full ml-2 hidden group-hover:flex transition-opacity whitespace-nowrap text-sm text-slate-400">
+              Add page
+            </span>
+          </button>
+          <button
+            className="p-1 ml-auto mr-2 text-muted-foreground hover:text-foreground"
+            onClick={() => setIsModalOpen(true)}
+          >
+            <List size={14} className="w-5 h-5" />
+          </button>
+        </div>
       </div>
       <AnimatePresence mode="wait">
         <motion.div
@@ -239,11 +302,10 @@ export const BrowserTabs: React.FC<BrowserTabsProps> = ({
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: -10 }}
           transition={{ duration: 0.2 }}
-          className=" h-[90vh] rounded-r-xl rounded-b-xl w-full bg-white overflow-hidden border border-slate-300 z-10 "
+          className=" h-[90vh] rounded-l-xl md:rounded-l-none rounded-r-xl rounded-b-xl w-full bg-white overflow-hidden border border-slate-300 z-10 "
         >
           <ChatClient
             title={activeTab?.title || ""}
-
             tabId={activeTab?.id || ""}
             notebookId={notebookId}
             onPageDelete={syncTabs}
