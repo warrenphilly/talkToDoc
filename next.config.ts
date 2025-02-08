b7bfe14f6da543e689ff848babff3fa30026dc6d
Warren Phillips <38818772+warrenphilly@users.noreply.github.com>
2025-02-08 09:32:41 -0500

    tester

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb'
+    },
+    responseLimit: '10mb'
+  }
 };
 
 export default nextConfig;
