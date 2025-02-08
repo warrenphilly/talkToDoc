94491497e64d31217107795215da2ac6d11c095a
Warren Phillips <38818772+warrenphilly@users.noreply.github.com>
2025-02-08 09:45:13 -0500

    best

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb'
-    },
-    responseLimit: '10mb'
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb'
+    }
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/convert/:path*',
+        headers: [
+          {
+            key: 'max-body-size',
+            value: '10mb'
+          }
+        ],
+      },
+    ]
   }
 };
 
