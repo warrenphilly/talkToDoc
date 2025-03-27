50579305f10b9a6f844e26f8dfa985ad4701eb82
Warren Phillips <38818772+warrenphilly@users.noreply.github.com>
2025-03-26 22:46:26 -0400

    trial

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -5,10 +5,10 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className=" flex flex-col items-center justify-center bg-blue-500 ">
+    <div className=" flex flex-col items-center justify-center bg-blue-500 h-screen p-6 py-16 overflow-y-auto">
   
 
-      <div className="w-full max-w-md bg-green-500 flex flex-col items-center justify-center h-screen p-6 py-16 overflow-y-auto">
+      <div className="w-full max-w-md bg-green-500 flex flex-col items-center justify-center ">
         <div className="">
           <Link
             href="/"
