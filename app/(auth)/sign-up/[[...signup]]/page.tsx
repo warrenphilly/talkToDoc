7480bb77733c4af4d6c8b64f21940f3e5564847e
warrenphilly <38818772+warrenphilly@users.noreply.github.com>
2025-03-27 10:49:52 -0400

    finishing touches

--- app/(auth)/sign-up/[[...signup]]/page.tsx
+++ app/(auth)/sign-up/[[...signup]]/page.tsx
@@ -8,24 +8,13 @@ export default function Page() {
     <div className=" flex flex-col items-center justify-center  w-full h-screen  bg-white overflow-y-auto  md:py-0 pb-24 md:pb-0">
   
 
-      <div className="w-full max-w-md flex flex-col items-center justify-center pt-8 mt-16 md:mt-0 md:pt-0 ">
-        <div className=" bg-blue-500 mt-44 pt-44 max-h-[50%]">
-          <Link
-            href="/"
-            className=" items-center gap-2 mb-8 transition-transform hover:scale-105 text-center flex justify-center"
-          >
-             <Image src="/zeLogo.svg" alt="logo" width={32} height={32} />
-            <span className="font-semibold text-[#94b347] text-xl">
-              Gammanotes
-            </span>
-          </Link>
           <SignUp
             redirectUrl="/"
             signInUrl="/sign-in"
           />
-        </div>
+      
 
-      </div>
+    
 
       <div className="mt-10 text-center max-w-sm">
         <p className="text-xs text-slate-500 flex flex-col items-center justify-center">
