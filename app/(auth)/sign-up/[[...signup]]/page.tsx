ead50acde2339c93139d377be7faf5f400c765b6
warrenphilly <38818772+warrenphilly@users.noreply.github.com>
2025-03-27 12:18:36 -0400

    trial

--- app/(auth)/sign-up/[[...signup]]/page.tsx
+++ app/(auth)/sign-up/[[...signup]]/page.tsx
@@ -5,14 +5,14 @@ export default function Page() {
   return (
     <div className=" w-full h-screen overflow-y-auto bg-white">
       <div className="flex min-h-full items-center justify-center p-4">
-        <div className="w-fit h-fit overflow-visible pt-6 bg-green-500 items-center justify-center pb-44">
+        <div className="w-fit h-fit overflow-visible pt-6 items-center justify-center pb-44">
           <SignUp
             redirectUrl="/"
             signInUrl="/sign-in"
             appearance={{
               elements: {
                 rootBox: "w-full overflow-visible",
-                card: "w-full shadow-none p-0 overflow-visible",
+                card: "w-full shadow-none overflow-visible",
                 formButtonPrimary: "bg-blue-600 hover:bg-blue-700 text-white",
               },
             }}
