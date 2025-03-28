2740c44f0a82927e16f4f4b8ac8c6edc7855dff6
warrenphilly <38818772+warrenphilly@users.noreply.github.com>
2025-03-28 09:25:02 -0400

    solution loading

--- app/(auth)/sign-up/[[...signup]]/page.tsx
+++ app/(auth)/sign-up/[[...signup]]/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div className=" w-full h-screen overflow-y-auto bg-white">
       <div className="flex min-h-full items-center justify-center">
-        <div className="w-fit h-fit overflow-visible  items-center justify-center py-44 bg-green-500">
+        <div className="w-fit h-fit overflow-visible  items-center justify-center py-32 bg-green-500">
           <SignUp
             redirectUrl="/auth-redirect"
             signInUrl="/sign-in"
